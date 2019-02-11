f6feab62360d32131b6aeed0b78fe3cbdef0a2b1
MuellerSeb <mueller.seb@posteo.de>
2019-02-11 13:35:44 +0100

    update LICENSE, setup

--- setup.py
+++ setup.py
@@ -1,30 +1,45 @@
-#!/usr/bin/env python
-"""
-AnaFlow
-python libary containing analytical and semi-analytical solutions
-for pumping-test:
-    thiem - steady-state, confiend- and homogeneous aquifer
-    theis - transient-state, confiend- and homogeneous aquifer
-    ext_thiem2D - steady-state, confiend- and heterogeneous aquifer in 2D
-    ext_thiem3D - steady-state, confiend- and heterogeneous aquifer in 3D
-    ext_theis2D - transient-state, confiend- and heterogeneous aquifer in 2D
-    ext_theis3D - transient-state, confiend- and heterogeneous aquifer in 3D
-    diskmodel - transient-state, confiend- and heterogeneous aquifer in 2D
-by Sebastian Mueller 2018
-"""
+# -*- coding: utf-8 -*-
+""" AnaFlow - analytical solutions for the groundwater-flow equation"""
+
+import os
+import codecs
+import re
 
 from setuptools import setup, find_packages
-from anaflow import __version__ as VERSION
+
+
+# find __version__ ############################################################
+
+
+def read(*parts):
+    """read file data"""
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    """find version without importing module"""
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+###############################################################################
+
 
 DOCLINES = __doc__.split("\n")
-README = open('README.md').read()
+README = open("README.md").read()
 CLASSIFIERS = """\
 Development Status :: 3 - Alpha
 Intended Audience :: Developers
 Intended Audience :: End Users/Desktop
 Intended Audience :: Science/Research
-License :: OSI Approved :: \
-GNU Lesser General Public License v3 or later (LGPLv3+)
+License :: OSI Approved :: GNU General Public License v3 (GPLv3)
 Natural Language :: English
 Operating System :: MacOS
 Operating System :: MacOS :: MacOS X
@@ -40,6 +55,8 @@ Topic :: Software Development
 Topic :: Utilities
 """
 
+VERSION = find_version("anaflow", "_version.py")
+
 setup(
     name='anaflow',
     version=VERSION,
@@ -50,8 +67,8 @@ setup(
     long_description_content_type="text/markdown",
     author="Sebastian Mueller",
     author_email="sebastian.mueller@ufz.de",
-    url='https://github.com/MuellerSeb/AnaFlow',
-    license='LGPL -  see LICENSE',
+    url='https://github.com/GeoStat-Framework/AnaFlow',
+    license='GPL - see LICENSE',
     classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
     platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
     include_package_data=True,
