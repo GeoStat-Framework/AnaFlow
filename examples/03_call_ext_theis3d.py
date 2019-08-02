81871eff06ec9d1b133c792261e1ae4a5db22fa0
MuellerSeb <mueller.seb@posteo.de>
2019-08-02 17:34:17 +0200

    update examples

--- examples/03_call_ext_theis3d.py
+++ examples/03_call_ext_theis3d.py
@@ -5,6 +5,7 @@ from anaflow import theis, ext_theis3D
 from anaflow.tools.special import aniso
 
 
+time_labels = ["10 s", "10 min", "10 h"]
 time = [10, 600, 36000]                 # 10s, 10min, 10h
 rad = np.geomspace(0.05, 4)             # radial distance from the pumping well in [0, 4]
 var = 0.5                               # variance of the log-conductivity
@@ -20,7 +21,7 @@ L = 1.0                                 # vertical extend of the aquifer
 head_Kefu = theis(time=time, rad=rad, T=Kefu*L, S=S, Qw=Qw)
 head_KH = theis(time=time, rad=rad, T=KH*L, S=S, Qw=Qw)
 head_ef = ext_theis3D(time=time, rad=rad, KG=KG, sig2=var, corr=corr, e=e, S=S, Qw=Qw, L=1)
-
+time_ticks=[]
 for i, step in enumerate(time):
     if i == 0:
         label_TG = "Theis($K_{efu}$)"
@@ -31,8 +32,15 @@ for i, step in enumerate(time):
     plt.plot(rad, head_Kefu[i], label=label_TG, color="C"+str(i), linestyle="--")
     plt.plot(rad, head_KH[i], label=label_TH, color="C"+str(i), linestyle=":")
     plt.plot(rad, head_ef[i], label=label_ef, color="C"+str(i))
+    time_ticks.append(head_Kefu[i][-1])
 
 plt.xlabel("r in [m]")
 plt.ylabel("h in [m]")
 plt.legend()
+ylim = plt.gca().get_ylim()
+plt.gca().set_xlim([0, rad[-1]])
+ax2 = plt.gca().twinx()
+ax2.set_yticks(time_ticks)
+ax2.set_yticklabels(time_labels)
+ax2.set_ylim(ylim)
 plt.show()
