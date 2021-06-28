b618afd37e0ee155eac01c64689efbd255c3b6b5
JarnoHerr <jarnoherrmann99@gmail.com>
2021-06-28 19:03:01 +0200

    Made the Neuman sloution from Moenche et al. 1996

--- anaflow/flow/Neuman.py
+++ anaflow/flow/Neuman.py
@@ -7,36 +7,31 @@ Anaflow subpackage providing the Neuman equation for homogeneous aquifer.
 The following functions are provided
 
 .. autosummary::
-
+   neuman_unconfined_laplace
+   neuman_unconfined
 """
 # pylint: disable=C0103
 import numpy as np
-from scipy.special import k0, k1
-from scipy.integrate import quad as integ
-
-import matplotlib.pyplot as plt
-
+from scipy.special import k0
 from anaflow.tools.laplace import get_lap_inv
-from anaflow.flow.laplace import grf_laplace
-from anaflow.tools.special import Shaper, sph_surf
+from anaflow.tools.special import Shaper
+from scipy.optimize import fsolve
 
 __all__ = []
 
 
-def neuman(
-        s,
-        rad,
-        storage,
-        transmissivity,
-        sat_thickness=52.0,
-        rate=-1e-4,
-        d=0.6,
-        l=12.6,
-        rw=0.1,
-        kz=0.14,
-        kr=0.23,
-        ss=4.27e-5,
-        n_numbers=52,
+def neuman_unconfined_laplace(
+    s,
+    rad,
+    storage,
+    transmissivity,
+    rate,
+    sat_thickness=52.0,
+    screen_size=11.88,
+    well_depth=12.6,
+    kd=0.61,
+    specific_yield=0.26,
+    n_numbers=10,
 ):
     """
         The Neuman solution.
@@ -45,7 +40,8 @@ def neuman(
         Parameters
         ----------
     s : :class:`numpy.ndarray`
-        Array with all time-points where the function should be evaluated in the Laplace space.
+        Array with all "Laplace-space-points" where the function should
+        be evaluated in the Laplace space.
     rad : :class:`numpy.ndarray`
         Array with all radii where the function should be evaluated.
     storage : :class:`float`
@@ -56,23 +52,19 @@ def neuman(
         Saturated thickness of the aquifer.
     rate : :class:`float`, optional
         Pumpingrate at the well. Default: -1e-4
-    d : :class:`float`, optional
-        Vertical distance from initial water table to top of
-        pumped well screen
-    l : :class:`float`, optional
+    screen_size : :class:`float`, optional
+        Vertical length of the observation screen
+    well_depth : :class:`float`, optional
         Vertical distance from initial water table to bottom
         of pumped well screen
-    rw : :class:`float`, optional
-        Outside radius of the pumped well screen
-    kz : :class:`float`, optional
-        Hydraulic conductivity in the vertical direction
-    kr : :class:`float`, optional
-        Hydraulic conductivity in the horizontal direction
-    Ss : :class:`float`, optional
-        specific storage
+    kd : :class:`float`, optional
+        Dimensionless parameter for the conductivity.
+        Kz/Kr : vertical conductivity divided by horizontal conductivity
+    specific_yield: :class:`float`, optional
+        specific yield
     """
-    z = float(sat_thickness - l)
-    kd = kz / kr
+    z = sat_thickness - well_depth
+    d = well_depth - screen_size
     s = np.squeeze(s).reshape(-1)
     rad = np.squeeze(rad).reshape(-1)
     res = np.zeros(s.shape + rad.shape)
@@ -80,49 +72,61 @@ def neuman(
     for si, se in enumerate(s):
         for ri, re in enumerate(rad):
             if re < np.inf:
-                for n in range(n_numbers):
-                    epsilon_n = np.pi / 2 + n * np.pi
-                    betaw = kd * rw ** 2
-                    qn = ((epsilon_n ** 2) * betaw + se) ** 0.5
-                    rd = re / rw
-                    E = k0(qn * rd) * np.cos(epsilon_n * (z / sat_thickness)) / (
-                            (
-                                    np.sin(epsilon_n * (1 - (d / sat_thickness)))
-                                    - np.sin(epsilon_n * (1 - (l / sat_thickness)))
-                            )
-                            / (qn * k1(qn) * (epsilon_n + 0.5 * np.sin(2 * epsilon_n)))
-                        )
-                    A = k0(qn) * (
-                            (
-                                    np.sin(epsilon_n * (1 - (d / sat_thickness)))
-                                    - np.sin(epsilon_n * (1 - (l / sat_thickness)))
-                            )
-                            ** 2
-                            / (
-                                    epsilon_n
-                                    * qn
-                                    * k1(qn)
-                                    * (epsilon_n + 0.5 * np.sin(2 * epsilon_n))
-                            )
+                rd = re / sat_thickness
+                betaw = kd * (rd ** 2)
+                righthand = se / (
+                    ((storage / specific_yield) * betaw) + se / 1e9
+                )
+                if righthand > 10:
+                    epsilon_n = np.arange(
+                        np.pi / 2, (n_numbers + 3) * np.pi, np.pi
                     )
-                E = 2 * E
-                A = (2 / ((l - d) / sat_thickness)) * A
-                sw = storage
-                wd = np.pi * rw ** 2 / (2 * np.pi * rw ** 2 * ss * (l - d))
-                res[si, ri] = (2 * E) / (se * ((l - d) / sat_thickness) * (1 + wd * se * (A + sw)))
+                else:
+                    f = lambda eps: eps * np.tan(eps) - righthand
+                    eps_0 = fsolve(
+                        f, np.arange(np.pi / 2, (n_numbers + 3) * np.pi, np.pi)
+                    )
+                    epsilon_n = eps_0[0:n_numbers]
+                    print(epsilon_n)
+                for n in range(min(n_numbers, len(epsilon_n))):
+                    xn = (betaw * (epsilon_n[n] ** 2) + se) ** 0.5
+                    if epsilon_n[n] == 0:
+                        continue
+                    else:
+                        res[si, ri] = (
+                            2
+                            * k0(xn)
+                            * (
+                                np.sin(
+                                    epsilon_n[n] * (1 - (d / sat_thickness))
+                                )
+                                - np.sin(
+                                    epsilon_n[n]
+                                    * (1 - (well_depth / sat_thickness))
+                                )
+                            )
+                            * np.cos(epsilon_n[n] * (z / sat_thickness))
+                        ) / (
+                            se
+                            * ((well_depth - d) / sat_thickness)
+                            * (
+                                0.5 * epsilon_n[n]
+                                + 0.25 * np.sin(2 * epsilon_n[n])
+                            )
+                        )
     res *= rate / (2 * np.pi * transmissivity)
     return res
 
 
-def neuman_from_laplace(
-        time,
-        rad,
-        storage,
-        transmissivity,
-        rate=-1e-4,
-        h_bound=0.0,
-        struc_grid=True,
-        lap_kwargs=None,
+def neuman_unconfined(
+    time,
+    rad,
+    storage,
+    transmissivity,
+    rate,
+    h_bound=0.0,
+    struc_grid=True,
+    lap_kwargs=None,
 ):
     """Neuman solution form laplace solution."""
     Input = Shaper(time, rad, struc_grid)
@@ -140,7 +144,7 @@ def neuman_from_laplace(
     }
     kwargs.update(lap_kwargs)
     res = np.zeros((Input.time_no, Input.rad_no))
-    lap_inv = get_lap_inv(neuman, **kwargs)
+    lap_inv = get_lap_inv(neuman_unconfined_laplace, **kwargs)
     # call the laplace inverse function (only at time points > 0)
     res[Input.time_gz, :] = lap_inv(Input.time[Input.time_gz])
     # reshaping results
@@ -148,15 +152,3 @@ def neuman_from_laplace(
     # add the reference head
     res += h_bound
     return res
-
-
-time = [10, 600, 36000]  # 10s, 10min, 10h
-rad = np.geomspace(0.1, 10)
-# default parameters
-T = 1e-4
-S = 1e-4
-Q = -1e-4
-
-neu = neuman_from_laplace(time, rad, storage=S, transmissivity=T)
-for i in range(len(time)):
-    plt.plot(rad, neu[i, :], color="C0", linestyle=":", linewidth=4)
